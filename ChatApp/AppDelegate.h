5a2b864d801db93106588c62dc3e628e3cff32e2
rewajkale <rewajkale@gmail.com>
2015-10-30 20:28:21 -0700

    Initial Commit

---
+++ ChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatApp
+//
+//  Created by Rewa on 30/10/15.
+//  Copyright (c) 2015 Rewa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatApp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  ChatApp
+//
+//  Created by Rewa on 30/10/15.
+//  Copyright (c) 2015 Rewa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
